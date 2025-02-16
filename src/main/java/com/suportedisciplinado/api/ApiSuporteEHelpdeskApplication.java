1d4f27bee9e2112a12c828562a14ade7ceaac67c
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-02-16 22:24:54 +0100

    init

---
+++ src/main/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.suportedisciplinado.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuporteEHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuporteEHelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suportedisciplinado.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSuporteEHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
