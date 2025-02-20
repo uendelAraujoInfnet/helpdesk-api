63bb4dd0e2370a6836ad0034f8a7773f8dbfb7a6
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-02-20 23:13:57 +0100

    exclude = {DataSourceAutoConfiguration.class}

--- src/main/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplication.java
+++ src/main/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplication.java
@@ -2,8 +2,9 @@ package com.suportedisciplinado.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiSuporteEHelpdeskApplication {
 
 	public static void main(String[] args) {
